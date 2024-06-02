476be5153bba01edd55d75b15b8fa6d4ee7920e6
Léo Carpentier <babidi@leo-pc>
2024-06-02 13:04:54 -0400

    Done

---
+++ withDP.py
@@ -0,0 +1,19 @@
+import time
+memo = {0: 0, 1: 1}
+def fib(n):
+    if n in memo:
+        return memo[n]
+    else:
+        result = fib(n - 1) + fib(n - 2)
+    memo[n] = result
+    return result   
+
+n = 20
+
+start = time.time()
+result = fib(n)
+end = time.time()
+elapsedTime = end - start
+
+print("Right way took ", elapsedTime, " seconds")
+print("Result for ", n, ": ", result)
---
+++ withoutDP.py
@@ -0,0 +1,17 @@
+import time
+def fib(n):
+    if n <= 2:
+        result = 1
+    else:
+        result = fib(n - 1) + fib(n - 2)
+    return result   
+
+n = 20
+
+start = time.time()
+result = fib(n)
+end = time.time()
+elapsedTime = end - start
+
+print("Wrong way took ", elapsedTime, " seconds")
+print("Result for ", n, ": ", result)
